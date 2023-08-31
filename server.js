fe87209d849c2e4ba2577e6de92d9d3f393a8cc2
Jeff Griffiths <jkgriffiths93@gmail.com>
2023-08-31 10:18:12 -0400

    Add front end

--- server.js
+++ server.js
@@ -2,6 +2,6 @@ const express = require('express');
 
 const app = express();
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
